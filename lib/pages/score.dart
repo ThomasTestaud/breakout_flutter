5f1473a8b60273cdd403ab4d3dbd7fc10cf749f8
ThomasTestaud <thomas.testaud@hotmail.fr>
2024-02-01 13:54:31 +0100

    ...

--- lib/pages/score.dart
+++ lib/pages/score.dart
@@ -22,28 +22,49 @@ class _ResultPageState extends State<ResultPage> {
         child: Padding(
           padding: const EdgeInsets.all(20),
           child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text("Fin de la partie !", style: TextStyle(fontSize: 25, color: Color.fromARGB(255, 199, 91, 28))),
-            Text("Vous avez obtenu ${widget.score} points !",
-                style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
-            Text(widget.score > 4 ? "Vous êtes un expert !" : widget.score > 3 ? 'Vous avez obtenu la mention "Ça va"' : 'Vous êtes nul...', style: TextStyle(fontSize: 20)),
-            const SizedBox(height: 20),
-            ClipRRect(
-            borderRadius: BorderRadius.circular(20), // Image border
-            child: SizedBox.fromSize(
-              size: const Size.fromRadius(150), // Image radius
-              child: Image.asset(widget.score > 4 ? 'img/great.gif' : widget.score > 3 ? 'img/good.gif' : 'img/bad.gif', fit: BoxFit.cover),
-            ),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Fin de la partie !",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 25, color: Color.fromARGB(255, 199, 91, 28))),
+              Text(
+                "Vous avez obtenu ${widget.score} points !",
+                textAlign: TextAlign.center,
+                style:
+                    const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                  widget.score > 4
+                      ? "Vous êtes un expert !"
+                      : widget.score > 3
+                          ? 'Vous avez obtenu la mention "Ça va"'
+                          : 'Vous êtes nul...',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 20)),
+              const SizedBox(height: 20),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20), // Image border
+                child: SizedBox.fromSize(
+                  size: const Size.fromRadius(150), // Image radius
+                  child: Image.asset(
+                      widget.score > 4
+                          ? 'img/great.gif'
+                          : widget.score > 3
+                              ? 'img/good.gif'
+                              : 'img/bad.gif',
+                      fit: BoxFit.cover),
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text("Rejouer"))
+            ],
           ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: const Text("Rejouer"))
-          ],
-        ),),
+        ),
       ),
     );
   }
